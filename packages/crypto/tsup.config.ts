e2526b5001908b7b09bc5986532bfd0bfe5a3147
vanderbr <11340250+vanderbr@users.noreply.github.com>
2025-12-07 23:03:55 +0100

    updaet tsup config

---
+++ packages/crypto/tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"], 
+  format: ["esm"],
+  dts: true,
+  splitting: false,
+  target: "es2022",
+
+  external: [
+    "@noble/curves",
+    "@noble/curves/*",
+    "@noble/hashes",
+    "@noble/hashes/*",
+    "@noble/post-quantum",
+    "@noble/post-quantum/*",
+  ],
+});
